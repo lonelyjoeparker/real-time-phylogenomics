fa3e94cbe571180a28b5dd58d74ab78a47bc2d25
lonelyjoeparker <joe@lonelyjoeparker.com>
2015-10-30 16:32:54 +0000

    stub class for a realtime analyzer

---
+++ src/com/lonelyjoeparker/realtimephylogenomics/RealtimeReadAnalyzer.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com.lonelyjoeparker.realtimephylogenomics;
+
+/**
+ * A simple class to monitor a directory, and (when new .fastq files appear)
+ * align, them and build a phylogeny
+ * @author <a href="http://github.com/lonelyjoeparker">@lonelyjoeparker</a>
+ * @since 30 Oct 2015
+ * @version 0.1
+ */
+public class RealtimeReadAnalyzer {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
